de4a057a26e9905491f0388ad198e75f2f68f2c5
y3hya <tmp.yahya1@gmail.com>
2021-09-20 01:14:14 +0500

    Created Date formatter function

---
+++ Statement.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from datetime import datetime
+import os
+
+def dateFormatter(Month, Frmt):
+    x = datetime.strftime(datetime.strptime(Month + ' 00:00:00', '%m/%d/%Y %H:%M:%S'), Frmt)
+    return x
+
+Month = dateFormatter('07/01/2021', '%Y-%m')
+# myPath = os.listdir(r'C:/Dropbox/OpSupport Team Folder/Cell Shop/Commission/AT&T Reports/' + datetime.Month(_, '%Y-%m') + '/Formatted') 
